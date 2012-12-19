4f3b06e7917c792172e6529eb58db4c3c5f1578c
Jono Xia <jono@fastmail.fm>
2012-12-19 11:48:02 -0800

    Realized that we don't want client submitting a form each time, since that results in page reload - should use a js post function instead. Wrote a skeleton click handler for the send button.

--- basic-server.js
+++ basic-server.js
@@ -1,29 +1,44 @@
 /* Import node's http module: */
 var http = require("http");
 
-/* This is the callback function that will be called each time a client (i.e.. a web browser) makes a request to our server. */
-var requestListener = function (req, res) {
+/* This is the callback function that will be called each time a
+ * client (i.e.. a web browser) makes a request to our server. */
+var requestListener = function (request, response) {
 
-  /* Req is an object containing various data about the client request
-   * - such as what URL the browser is requesting. */
-  console.log("Serving request type " + req.method
-              + " for url " + req.url);
+  /* Request is an http.ServerRequest object containing various data
+   * about the client request - such as what URL the browser is
+   * requesting. */
+  console.log("Serving request type " + request.method
+              + " for url " + request.url);
 
-  /* Res is an object containing methods for writing our response to
-   * the client.*/
-  res.writeHead(200, {'Content-Type': "text/plain"});
-  /* writeHead() tells our server what HTTP status code to send back to the client, and what headers to include on the response. */
-  res.end("Hello, World!");
-  /* Make sure to always call res.end() - Node will not send anything back to the client until you do. The string you pass to res.end() will be the body of the response - i.e. what shows up in the browser.*/
+  /* Response is an http.ServerRespone object containing methods for
+   * writing our response to the client. Documentation for both request
+   * and response can be found at 
+   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html*/
+  response.writeHead(200, {'Content-Type': "text/plain"});
+  /* writeHead() tells our server what HTTP status code to send back
+   * to the client, and what headers to include on the response. */
+  response.end("Hello, World!");
+  /* Make sure to always call response.end() - Node will not send
+   * anything back to the client until you do. The string you pass to
+   * response.end() will be the body of the response - i.e. what shows
+   * up in the browser.*/
 };
 
-/* Every server needs to listen on a port with a unique number. The standard port for HTTP servers is port 80, but that port is normally already claimed by another server and/or not accessible to user processes, so we'll use a higher port number that is not likely to be taken: */
+/* Every server needs to listen on a port with a unique number. The
+ * standard port for HTTP servers is port 80, but that port is
+ * normally already claimed by another server and/or not accessible to
+ * user processes, so we'll use a higher port number that is not
+ * likely to be taken: */
 var port = 8080;
 
-/* For now, since you're running this server on your local machine, we'll have it listen on the IP address 127.0.0.1, which is a special address that always refers to localhost. */
+/* For now, since you're running this server on your local machine,
+ * we'll have it listen on the IP address 127.0.0.1, which is a
+ * special address that always refers to localhost. */
 var ip = "127.0.0.1";
 
-/* Use node's http module to create a server and start it listening on the given port and IP. */
+/* Use node's http module to create a server and start it listening on
+ * the given port and IP. */
 var server = http.createServer(requestListener);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
@@ -32,6 +47,9 @@ server.listen(port, ip);
      node basic-server.js
  *  on the command line.
 
- * To connect to the server, load http://127.0.0.1:8080 in your web browser.
+ * To connect to the server, load http://127.0.0.1:8080 in your web
+ * browser.
 
- * server.listen()  will continue running as long as there is the possibility of serving more requests. To stop your server, hit Ctrl-C on the command line. */
+ * server.listen() will continue running as long as there is the
+ * possibility of serving more requests. To stop your server, hit
+ * Ctrl-C on the command line. */
--- pages/basic-client.js
+++ pages/basic-client.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   var chatLog = $("#chat-log");
   var pollingInterval = 1000;
 
+  chatLog.append("<p>Waiting for Node server...</p>");
+
   /* Periodically poll the server to see if there are new chat
    * messages: */
   window.setInterval(function() {
@@ -10,5 +12,21 @@ $(document).ready(function() {
      * How to do that? You may want to take a look at the jquery
      * ajax documentation at: http://api.jquery.com/category/ajax/
      */
+
   }, pollingInterval);
+
+
+  /* When user clicks the "Send" button, get the input field values
+   * and send them to the server: */
+  $("#send-button").click(function() {
+    var serverUrl = "http://127.0.0.1:8080/send";
+
+    var username = $("#username").val();
+    var message = $("#message").val();
+    /* In this function you should POST the username and message to
+     * the server URL. The jquery ajax documentation will also be
+     * handy here. */
+
+  });
 });
+
