2122ab424d45b35f08331fd593a0147a539803e9
Hack Reactor Students <github@hackreactor.com>
2013-08-26 16:53:37 -0700

    handling POST requests, working on GET requests

--- request-handler.js
+++ request-handler.js
@@ -3,7 +3,29 @@
  * basic-server.js.  So you must figure out how to export the function
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
+var messages = [];
 
-var handleRequest = function(request, response) {
+var handlePostRequest = function(request){
+  var someData = '';
 
+  request.on('data', function(data){
+     someData+=data;
+  });
+
+  request.on('end', function(){
+    var parsedData = JSON.parse(someData);
+    var messageObj = {};
+    messageObj.username = parsedData.username;
+    messageObj.text = parsedData.text;
+    messageObj.roomname = parsedData.roomname;
+    messageObj.createdAt = new Date();
+    messages.push(messageObj);
+  });
 };
+
+var handleGetRequest = function(request){
+  
+};
+
+exports.handlePostRequest = handlePostRequest;
+exports.handleGetRequest = handleGetRequest;
